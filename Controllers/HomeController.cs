339380eac54dd595d219671fe436f64154898687
Priyal-Bhesaniya <priyalbhesaniya@gmail.com>
2025-03-17 20:07:14 +0530

    added two anotheer view to my homecontroller and as well as my file .cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,15 @@ namespace HopeConnect.Controllers
             return View();
         }
 
+        public IActionResult Contactus()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
