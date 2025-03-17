302735e7d6518626e78f99c27fb598bbe1211a3d
Priyal-Bhesaniya <priyalbhesaniya@gmail.com>
2025-03-17 18:45:23 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HopeConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
