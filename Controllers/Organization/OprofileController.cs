7743d99d4647a85782ed5b512b289ab7f4652148
Viral Shingadia <119102691+Viral1702@users.noreply.github.com>
2025-04-17 00:40:15 +0530

    Created a controller for profile page

--- Controllers/Organization/OprofileController.cs
+++ Controllers/Organization/OprofileController.cs
@@ -1,13 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HopeConnect.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HopeConnect.Controllers.Organization
 {
     public class OprofileController : Controller
     {
+        private readonly OrganizationModel _organizationModel;
+
+        // Use constructor injection for IConfiguration
+        public OprofileController(IConfiguration configuration)
+        {
+            _organizationModel = new OrganizationModel(configuration); // Passing IConfiguration to OrganizationModel
+        }
+
+        // GET: Oprofile
+        [HttpGet]
         public IActionResult Oprofile()
         {
-            ViewData["ActivePage"] = "Oprofile";
-            return View("~/Views/Organization/Oprofile.cshtml");
+            // Get OrgEmail from session
+            var email = HttpContext.Session.GetString("OrgEmail");
+            if (string.IsNullOrEmpty(email))
+                return RedirectToAction("Login", "Home");
+
+            // Fetch organization details using the email
+            var organization = _organizationModel.GetByEmail(email);
+            if (organization == null)
+                return RedirectToAction("Login", "Home");
+
+            // Pass the organization info to the view
+            return View("~/Views/Organization/Oprofile.cshtml", organization);
+        }
+
+        // POST: Oprofile (for updating the organization details)
+        [HttpPost]
+        public IActionResult Oprofile(OrganizationModel updatedOrganization)
+        {
+            // Get OrgEmail from session
+            var email = HttpContext.Session.GetString("OrgEmail");
+            if (string.IsNullOrEmpty(email))
+                return RedirectToAction("Login", "Home");
+
+            // Fetch organization details using the email
+            var organization = _organizationModel.GetByEmail(email);
+            if (organization == null)
+                return RedirectToAction("Login", "Home");
+
+            // Update organization info
+            organization.Name = updatedOrganization.Name;
+            organization.MobileNo = updatedOrganization.MobileNo;
+            organization.Password = updatedOrganization.Password;
+
+            // Update in the database
+            //bool isUpdated = _organizationModel.UpdateOrganizationProfile(organization);
+
+            // Set a message for success/failure
+            //TempData["Message"] = isUpdated ? "Profile updated successfully." : "Profile update failed.";
+
+            return RedirectToAction("Oprofile");
         }
     }
 }
