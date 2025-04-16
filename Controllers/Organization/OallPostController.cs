dd11e293acc2c7c028a81de4ab2088918591fe81
Viral Shingadia <119102691+Viral1702@users.noreply.github.com>
2025-04-17 00:41:11 +0530

    created a controller for all posts

--- Controllers/Organization/OallPostController.cs
+++ Controllers/Organization/OallPostController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HopeConnect.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HopeConnect.Controllers.Organization
 {
     public class OallPostController : Controller
     {
         public IActionResult OallPost()
+
+
+
         {
             ViewData["ActivePage"] = "OallPost";
-            return View("~/Views/Organization/OallPost.cshtml");
+            PostModel postModel = new PostModel();
+            List<PostModel> posts = postModel.GetAllPosts();
+            return View("~/Views/Organization/OallPost.cshtml", posts);
+
+
+
+
         }
     }
 }
